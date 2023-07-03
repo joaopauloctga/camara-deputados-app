8903a3845e83293691b1f78791a3ab712c450439
Joao Paulo Constantino <joaopauloctga@gmail.com>
2023-07-02 21:02:37 -0300

    wip

--- src/pages/partidos/index.js
+++ src/pages/partidos/index.js
@@ -1,19 +1,22 @@
 import ProfilePhoto from '@/components/deputado/ProfilePhoto';
 import PanelSeeMore from '@/components/panel-see-more/panel-see-more';
+import Panel from '@/components/panel/panel';
+import { faHouse } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Image from 'next/image';
 import React, { useState } from 'react';
 
 function PartidoDetails({id, nome, sigla, status, urlLogo, lideres, parlamentares}) {
-  const sizeLiderImage = lideres.length > 2 ? 'xs' : 'sm';
-  return <div className='flex flex-col lg:flex-row flex-wrap justify-center'>
-    <div className='w-full lg:w-2/6'>
-      <div className='flex flex-col items-center'>
-        <Image alt={nome} src={`/logos/partidos/${sigla.toLowerCase()}.png`} width={400} height={300} />
-        <h3 className='t3 t-primary'>{nome}</h3>
-        <h3 className='t4 t-primary'>{sigla}</h3>
+  return <div className='flex flex-col lg:flex-row flex-wrap p-2'>
+    <div className='w-full lg:w-3/6'>
+      <div className='flex flex-wrap items-center justify-center'>
+        <div className='w-full text-center'>
+          <Image className='mx-auto' alt={nome} src={`/logos/partidos/${sigla.toLowerCase()}.png`} width={300} height={300} />
+          {/* <FontAwesomeIcon style={{fontSize: '120px'}} className='t-primary' icon={faHouse} /> */}
+        </div>
       </div>
     </div>
-    <div className='w-full lg:w-2/6 text-center'>
+    {/* <div className='w-full lg:w-2/6 text-center'>
       <h3 className='t3 t-primary mb-2'>Lideres do partido</h3>
       {lideres.length === 0 && <h3>Nenhum lider definido</h3>}
       <PanelSeeMore maxHeight={400}>
@@ -21,12 +24,12 @@ function PartidoDetails({id, nome, sigla, status, urlLogo, lideres, parlamentare
           {lideres.map(p => <div className='w-1/2'><ProfilePhoto size={sizeLiderImage} name={p.nome} foto={p.urlFoto} /></div>)}
         </div>
       </PanelSeeMore>
-    </div>
-    <div className='w-full lg:w-2/6'>
+    </div> */}
+    <div className='w-full lg:w-3/6'>
       <h3 className='t3 t-primary text-center mb-2'>Membros do partido ({parlamentares.length})</h3>
       <PanelSeeMore maxHeight={400}>
         <div className='flex flex-wrap'>
-          {parlamentares.map(p => <div className='w-1/3'><ProfilePhoto size='xs' name={p.nome} foto={p.urlFoto} /></div>)}
+          {parlamentares.map(p => <div className='w-1/4'><ProfilePhoto size='xs' name={p.nome} foto={p.urlFoto} /></div>)}
         </div>
       </PanelSeeMore>
     </div>
@@ -34,18 +37,28 @@ function PartidoDetails({id, nome, sigla, status, urlLogo, lideres, parlamentare
 }
 
 function FiltroPartidos() {
-  return <div className='mb-2 w-full border border-color-1 rounded-sm'>
-    <ul className='flex flex-wrap items-center'>
-      <li className='p-1 px-4 bg-4 mr-4'>Ordernar por</li>
-      <li className='mr-2'>
-        <input type='checkbox' className='form-check' />
-        <label> Maioria</label>
-      </li>
-      <li className='mr-2'>
-        <input type='checkbox' className='form-check' />
-        <label> Minoria</label>
-      </li>
-    </ul>
+  return <div className='rounded-sm border border-color-1 p-1'>
+    <div>
+      <h3 className='text-sm t-primary'>Order por</h3>
+      <ul>
+        <li className=''>
+          <input type='checkbox' id='mais-deputados' className='form-check' />
+          <label htmlFor='mais-deputados'>Com mais deputados</label>
+        </li>
+        <li className=''>
+          <input type='checkbox' id='mais-deputados' className='form-check' />
+          <label htmlFor='mais-deputados'>Mais Mulheres</label>
+        </li>
+        <li className=''>
+          <input type='checkbox' id='mais-deputados' className='form-check' />
+          <label htmlFor='mais-deputados'>Mais Homens</label>
+        </li>
+        <li className=''>
+          <input type='checkbox' id='mais-deputados' className='form-check' />
+          <label htmlFor='mais-deputados'>Mais Projetos de Lei</label>
+        </li>
+      </ul>
+    </div>
   </div>
 }
 
@@ -63,13 +76,19 @@ const PartidosList = () => {
   }, []);
 
   return (
-    <div className="flex flex-wrap -mx-4">
-      <FiltroPartidos />
-      {partidosList.map((partido) => (
-        <div key={partido.id} className="w-full mb-4 p-3 rounded-sm border border-color-1">
-          <PartidoDetails {...partido} />
-        </div>
-      ))}
+    <div className="flex flex-wrap">
+      <div className='w-full lg:w-2/12'>
+        <FiltroPartidos />
+      </div>
+      <div className='w-full lg:w-10/12 pl-2'>
+        {partidosList.map((partido) => (
+          <div className='mb-4' key={partido.id}>
+            <Panel right title={partido.nome}>
+              <PartidoDetails {...partido} />
+            </Panel>
+          </div>        
+        ))}
+      </div>
     </div>
   );
 };
